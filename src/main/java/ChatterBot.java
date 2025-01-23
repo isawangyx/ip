b8ccd9209a22f3ecad77b9ef41d5a0d8ed212f91
isawangyx <yanxiangw04@gmail.com>
2025-01-24 01:00:21 +0800

    Add support for list

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class ChatterBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm ChatterBot");
         System.out.println("What can I do for you?");
 
@@ -12,9 +15,16 @@ public class ChatterBot {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("added: " + userInput);
             }
 
-            System.out.println(userInput);
         }
     }
 }
