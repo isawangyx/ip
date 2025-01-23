d78a6ea532576b8b17bfe53fbe5283862c3f89e2
isawangyx <yanxiangw04@gmail.com>
2025-01-24 00:52:46 +0800

    Add support for echo

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class ChatterBot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm ChatterBot");
         System.out.println("What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(userInput);
+        }
     }
 }
