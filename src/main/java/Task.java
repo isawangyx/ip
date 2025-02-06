1d8d10914acca72aae254990414be6f520a2304a
Isabel <yanxiangw04@gmail.com>
2025-02-06 11:22:48 +0800

    Add support for saving tasks in hard disk

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
@@ -6,6 +6,36 @@ public class Task {
         this.description = description;
         this.isDone = false;
     }
+
+    public static Task fromFileFormat(String line) {
+        String[] parts = line.split(" \\| ");
+        if (parts.length < 3) return null;
+
+        String type = parts[0];
+        boolean isDone = parts[1].equals("1");
+        String description = parts[2];
+
+        switch (type) {
+            case "T":
+                Task todo = new Todo(description);
+                if (isDone) todo.markAsDone();
+                return todo;
+            case "D":
+                if (parts.length < 4) return null;
+                Task deadline = new Deadline(description, parts[3]);
+                if (isDone) deadline.markAsDone();
+                return deadline;
+            case "E":
+                if (parts.length < 5) return null;
+                Task event = new Event(description, parts[3], parts[4]);
+                if (isDone) event.markAsDone();
+                return event;
+            default:
+                return null;
+        }
+    }
+
+    public abstract String toFileFormat();
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // Mark done task with X
