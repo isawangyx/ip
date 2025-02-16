19348be40cd50d7267ebffebff7d7e203207a9a4
Isabel <yanxiangw04@gmail.com>
2025-02-17 01:31:16 +0800

    Improve unknown command handling to prompt better user input

--- src/main/java/chatterbot/exceptions/UnknownCommandException.java
+++ src/main/java/chatterbot/exceptions/UnknownCommandException.java
@@ -2,6 +2,16 @@ package chatterbot.exceptions;
 
 public class UnknownCommandException extends ChatterBotException {
     public UnknownCommandException() {
-        super("Looks like you got creative with the input. Let’s stick to what I know for now!");
+        super("Looks like you got creative with the input." + System.lineSeparator()
+                + "Let’s try one of the following commands:" + System.lineSeparator()
+                + "list" + System.lineSeparator()
+                + "todo <desc>" + System.lineSeparator()
+                + "deadline <desc> /by <date>" + System.lineSeparator()
+                + "event <desc> /from <start> /to <end>" + System.lineSeparator()
+                + "mark <num>" + System.lineSeparator()
+                + "unmark <num>" + System.lineSeparator()
+                + "delete <num>" + System.lineSeparator()
+                + "find <keyword>" + System.lineSeparator()
+                + "bye (to exit the program)");
     }
 }
